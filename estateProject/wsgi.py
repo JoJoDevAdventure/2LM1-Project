92cbcd5285b0ebc406a7bff3dd23f49076ab2822
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2022-11-14 20:20:51 +0100

    fix

--- estateProject/wsgi.py
+++ estateProject/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'estateProject.settings')
 
 application = get_wsgi_application()
+DJANGO_PATH =  os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+sys.path.append(DJANGO_PATH)
