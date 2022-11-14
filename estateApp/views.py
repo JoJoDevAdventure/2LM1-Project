1e20180bd828415b7f42372de454c72677e9068d
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2022-11-14 15:12:14 +0100

    +back-end support

--- estateApp/views.py
+++ estateApp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def product(request):
+    return render(request, 'product.html')
+
 def index(request):
     return render(request, 'index.html')
