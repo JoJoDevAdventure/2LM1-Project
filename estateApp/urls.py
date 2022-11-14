f5124ca7c22e9b7a24adf93b645ccf9ec4079eed
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2022-11-14 18:53:35 +0100

    + Back-end support v3

--- estateApp/urls.py
+++ estateApp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('templates/product.html', views.product, name='product'),
     path('templates/index.html', views.index, name='index'),
+    path('templates/login.html', views.login, name='login'),
+    path('templates/register.html', views.register, name='register'),
 ]
