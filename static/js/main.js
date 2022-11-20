76f0501cd98aa0d35446ea443689e3fec0d3728e
reverie-kyrene <syr.zanina@gmail.com>
2022-11-20 23:06:53 +0100

    index

--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,66 @@
 function scrollHeader(){
-    const header= document.getElementById('header')
-    // When the scroll is greater than 50 viewport height, add the scroll
-    if (this.scrollY>= 50) header.classList.add('scroll-header'); else header.classList.remove
+  const header= document.getElementById('header')
+  // When the scroll is greater than 50 viewport height, add the scroll
+  if (this.scrollY>= 50) header.classList.add('scroll-header'); else header.classList.remove
 
 }
 window.addEventListener('scroll', scrollHeader)
 
 //SWIPER POPULAR
 var swiperPopular = new Swiper(".popular__container", {
-    spaceBetween: 32,
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    loop: true,
-    
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  loop: true,
+  lazyLoading: true,
+
+  
+  navigation: {
+    nextEl: "#nav-right",
+    prevEl: "#nav-left",
+  },
+  keyboard: {
+    enabled: true,
+  },
+  pagination: {
+    el: (".popular__container .swiper-custom-pagination"),
+    clickable: true,
+    renderBullet: function(index, className) {
+      return `<div class=${className}>
+      <span class="number">${index + 1}</span>
+      <span class="line"></span>
+      </div>`
+    }
+  }
 });
+
+var swiper2 = new Swiper(".destinations", {
+slidesPerView: 'auto',
+spaceBetween: 32,
+grabCursor: true,
+centeredSlides: true,
+loop: true,
+lazyLoading: true,
+
+
+navigation: {
+  nextEl: "#nav-right",
+  prevEl: "#nav-left",
+},
+keyboard: {
+  enabled: true,
+},
+pagination: {
+  el: (".destinations .swiper-custom-pagination"),
+  clickable: true,
+  renderBullet: function(index, className) {
+    return `<div class=${className}>
+    <span class="number">${index + 1}</span>
+    <span class="line"></span>
+    </div>`
+  }
+}
+
+});
+
